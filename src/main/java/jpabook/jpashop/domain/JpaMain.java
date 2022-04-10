1d9fb68aa419257902ad798c5c79ecfe77da4d40
Hayoon <gkdbssla97@gmail.com>
2022-04-10 19:12:54 +0900

    JPA_MAPPING

--- src/main/java/jpabook/jpashop/domain/JpaMain.java
+++ src/main/java/jpabook/jpashop/domain/JpaMain.java
@@ -1,9 +1,13 @@
-package hellojpa;
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JpaMain {
@@ -15,16 +19,23 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        try {
-            // 비영속
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
+        try{
+            // 저장
+            Member member = new Member();
+            member.setName("memberA");
+            em.persist(member);
 
-            //영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
+            List<Order> orders = new ArrayList<>();
+            orders.add(member);
+            member.setOrders(order);
+            em.persist(order);
 
-            System.out.println("======");
+            em.flush();
+            em.clear();
+
+            Order order1 = em.find(Order.class, order.getMember());
+            Member member1 = order1.getMember();
+            System.out.println(member1.getName());
 
             tx.commit();
         } catch (Exception e) {
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "CITY")
+    private String city;
+
+    @Column(name = "STREET")
+    private String street;
+
+    @Column(name = "ZIPCODE")
+    private String zipcode;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
